2cc0f88db815fe7cde0cc1bc8802e2bbb0a50b33
nicolassims <sableye.nico@gmail.com>
2021-03-22 16:38:06 -0700

    Modified response schema.

--- server/lib/events_spa/responses/response.ex
+++ server/lib/events_spa/responses/response.ex
@@ -8,12 +8,15 @@ defmodule EventsSpa.Responses.Response do
     field :event_id, :id
 
     timestamps()
+
+    belongs_to :user, EventsSpa.Users.User
+    belongs_to :event, EventsSpa.Events.Event
   end
 
   @doc false
   def changeset(response, attrs) do
     response
-    |> cast(attrs, [:value])
-    |> validate_required([:value])
+    |> cast(attrs, [:value, :user_id, :event_id])
+    |> validate_required([:value, :user_id, :event_id])
   end
 end
