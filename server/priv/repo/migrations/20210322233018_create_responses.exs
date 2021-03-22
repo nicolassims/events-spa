83d2420515ad5280b75a3c3d9ecdd15fdc5a16cd
nicolassims <sableye.nico@gmail.com>
2021-03-22 16:33:51 -0700

    Disallowed null in response migration.

--- server/priv/repo/migrations/20210322233018_create_responses.exs
+++ server/priv/repo/migrations/20210322233018_create_responses.exs
@@ -3,14 +3,13 @@ defmodule EventsSpa.Repo.Migrations.CreateResponses do
 
   def change do
     create table(:responses) do
-      add :value, :integer
-      add :user_id, references(:users, on_delete: :nothing)
-      add :event_id, references(:events, on_delete: :nothing)
+      add :value, :integer, null: false
+      add :user_id, references(:users, on_delete: :nothing), null: false
+      add :event_id, references(:events, on_delete: :nothing), null: false
 
       timestamps()
     end
 
-    create index(:responses, [:user_id])
-    create index(:responses, [:event_id])
+    create index(:responses, [:user_id, :event_id])
   end
 end
