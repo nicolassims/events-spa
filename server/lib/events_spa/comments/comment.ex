c3679b25b4dc85ebed6c188bf9202f0c744e3e9d
nicolassims <sableye.nico@gmail.com>
2021-03-22 16:13:25 -0700

    Created COmments resource. Haven't modified migrations or schema or anything.

---
+++ server/lib/events_spa/comments/comment.ex
@@ -0,0 +1,19 @@
+defmodule EventsSpa.Comments.Comment do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "comments" do
+    field :body, :string
+    field :user_id, :id
+    field :post_id, :id
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(comment, attrs) do
+    comment
+    |> cast(attrs, [:body])
+    |> validate_required([:body])
+  end
+end
