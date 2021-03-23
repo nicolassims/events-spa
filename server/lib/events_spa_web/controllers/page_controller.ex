60437a57c63e3f0984f3783f657ad8b82195959a
nicolassims <sableye.nico@gmail.com>
2021-03-23 15:32:22 -0700

    Update pagecontroller for picture compatibility.

--- server/lib/events_spa_web/controllers/page_controller.ex
+++ server/lib/events_spa_web/controllers/page_controller.ex
@@ -1,7 +1,18 @@
 defmodule EventsSpaWeb.PageController do
   use EventsSpaWeb, :controller
 
+  alias EventsSpa.Photos
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    events = EventsSpa.Events.list_events()
+    |> EventsSpa.Events.load_reactions()
+    render(conn, "index.html", events: events)
+  end
+
+  def photo(conn, %{"hash" => hash}) do
+    {:ok, _name, data} = Photos.load_photo(hash)
+    conn
+    |> put_resp_content_type("iamge/jpeg")
+    |> send_resp(200, data)
   end
 end
