e35f0fd36f97777163267cb1b1158b96382c6d5f
nicolassims <sableye.nico@gmail.com>
2021-03-23 14:44:43 -0700

    Create Feed.js

---
+++ web-ui/src/Feed.js
@@ -0,0 +1,26 @@
+import { Row, Col, Card } from 'react-bootstrap';
+import { connect } from 'react-redux';
+
+function Event({event}) {
+  return (
+    <Col>
+      <Card>
+        <Card.Text>
+          Posted by {event.user.name}<br />
+          {event.body}
+        </Card.Text>
+      </Card>
+    </Col>
+  );
+}
+
+function Feed({events}) {
+  let cards = events.map((event) => <Event event={event} key={event.id} />);
+  return (
+    <Row>
+      { cards }
+    </Row>
+  );
+}
+
+export default connect(({events}) => ({events}))(Feed);
