fd5a4fdf9b4316db1784e77f52f62bb7f718a4fa
nicolassims <sableye.nico@gmail.com>
2021-03-22 17:13:52 -0700

    Fixed router to pipe API data properly. I love giving data a good piping.

--- server/lib/events_spa_web/router.ex
+++ server/lib/events_spa_web/router.ex
@@ -13,17 +13,13 @@ defmodule EventsSpaWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/", EventsSpaWeb do
-    pipe_through :browser
-
-    get "/", PageController, :index
-    resources "/users", UserController, except: [:new, :edit]
-    resources "/events", EventController, except: [:new, :edit]
-  end
-
   # Other scopes may use custom stacks.
   scope "/api/v1", EventsSpaWeb do
     pipe_through :api
+
+    resources "/users", UserController, except: [:new, :edit]
+    resources "/events", EventController, except: [:new, :edit]
+    resources "/comments", CommentController, except: [:new, :edit]
   end
 
   # Enables LiveDashboard only for development
