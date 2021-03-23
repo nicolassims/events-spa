dcabd855e4da363b8e56482d2613e705f6dea96a
nicolassims <sableye.nico@gmail.com>
2021-03-22 17:03:50 -0700

    Fixed spacing issue in comments migration.

--- server/priv/repo/migrations/20210322231231_create_comments.exs
+++ server/priv/repo/migrations/20210322231231_create_comments.exs
@@ -3,9 +3,9 @@ defmodule EventsSpa.Repo.Migrations.CreateComments do
 
   def change do
     create table(:comments) do
-      add :body, :string, null:false
-      add :user_id, references(:users, on_delete: :nothing), null:false
-      add :event_id, references(:events, on_delete: :nothing), null:false
+      add :body, :string, null: false
+      add :user_id, references(:users, on_delete: :nothing), null: false
+      add :event_id, references(:events, on_delete: :nothing), null: false
       #TODO: what does on_delete mean?
       timestamps()
     end
