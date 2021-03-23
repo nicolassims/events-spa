4c552f548215e57040d3d57f1120de2948207943
nicolassims <sableye.nico@gmail.com>
2021-03-22 17:02:24 -0700

    Changed port and url of production file for much later on down the road.

--- server/config/prod.exs
+++ server/config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :events_spa, EventsSpaWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "events-spa.freudiancreations.website", port: 8999],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
