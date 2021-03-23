a9fc067978013ccdb71278ad46f2c26b59234cc9
nicolassims <sableye.nico@gmail.com>
2021-03-23 15:18:49 -0700

    Added pictures to seeds.

--- server/priv/repo/seeds.exs
+++ server/priv/repo/seeds.exs
@@ -12,8 +12,23 @@
 
 alias EventsSpa.Repo
 alias EventsSpa.Users.User
+alias EventsSpa.Events.Event
+alias EventsSpa.Photos
 
-_aster = Repo.insert!(%User{name: "Aster Martin", email: "", photo_hash: "", password_hash: ""})
-_zoe = Repo.insert!(%User{name: "Zoe Tesla", email: "", photo_hash: "", password_hash: ""})
-_day = Repo.insert!(%User{name: "Hyun-Woo Day", email: "", photo_hash: "", password_hash: ""})
-_rose = Repo.insert!(%User{name: "Rose Smart", email: "", photo_hash: "", password_hash: ""})
+defmodule Inject do
+  def photo(name) do
+    photos = Application.app_dir(:events_spa, "priv/photos")
+    path = Path.join(photos, name)
+    {:ok, hash} = Photos.save_photo(name, path)
+    hash
+  end
+end
+
+hummingbird = Inject.photo("hummingbird.png")
+hydra = Inject.photo("hydra.jpeg")
+unicorn = Inject.photo("unicorn.jpg")
+
+_aster = Repo.insert!(%User{name: "Aster Martin", email: "", photo_hash: hummingbird, password_hash: ""})
+_zoe = Repo.insert!(%User{name: "Zoe Tesla", email: "", photo_hash: hydra, password_hash: ""})
+_day = Repo.insert!(%User{name: "Hyun-Woo Day", email: "", photo_hash: hydra password_hash: ""})
+_rose = Repo.insert!(%User{name: "Rose Smart", email: "", photo_hash: unicorn password_hash: ""})
