a57d9731079d6d25e00ef1788582ab73d3db0cf2
nicolassims <sableye.nico@gmail.com>
2021-03-22 17:02:06 -0700

    Changed username and passwords in config files.

--- server/config/dev.exs
+++ server/config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :events_spa, EventsSpa.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "events_spa",
+  password: "karayakaylar",
   database: "events_spa_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
