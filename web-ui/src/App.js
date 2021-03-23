5620f04f56d16ed902cc16d5e2674d439f40105e
nicolassims <sableye.nico@gmail.com>
2021-03-22 17:18:22 -0700

    Update App.js

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+
+async function fetchUsers() {
+  let text = await fetch("http://localhost:4000/api/v1/users", {});
+  let resp = await text.json();
+  return resp.data;
+}
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    if (users.length === 0) {
+      fetchUsers().then((xs) => setUsers(xs));
+    }
+  }, [users]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {users.map((uu) => (<li key={uu.id}>{uu.name}</li>))}
+      </ul>
     </div>
   );
 }
