dc8551d33829749423c7c5d545e71098992b6ada
nicolassims <sableye.nico@gmail.com>
2021-03-23 14:23:53 -0700

    Implementing react-router-dom.

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -1,12 +1,22 @@
 import { Container } from 'react-bootstrap';
+import { Switch, Route } from 'react-router-dom';
 
 import "./App.scss";
 import Users from "./Users";
+import Nav from "./Nav";
 
 function App() {
   return (
     <Container>
-      <Users />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <h1>EventsApp Feed</h1>
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+      </Switch>
     </Container>
   );
 }
