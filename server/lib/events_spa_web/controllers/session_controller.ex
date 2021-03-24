de88c38a3422c2648e0b8febfd822ad75259744d
nicolassims <sableye.nico@gmail.com>
2021-03-24 10:46:09 -0700

    Update session_controller.ex

--- server/lib/events_spa_web/controllers/session_controller.ex
+++ server/lib/events_spa_web/controllers/session_controller.ex
@@ -2,15 +2,21 @@ defmodule EventsSpaWeb.SessionController do
   use EventsSpaWeb, :controller
 
   def create(conn, %{"name" => name, "password" => password}) do
-    user = EventsSpa.Users.get_user_by_name!(name)
-    # TODO: Verify password
-    sess = %{
-      user_id: user.id,
-      name: user.name,
-      token: Phoenix.Token.sign(conn, "user_id", user.id)
-    }
-    conn
-    |> put_resp_header("content-type", "application/json; charset=UTF-8")
-    |> send_resp(:created, Jason.encode!(sess))
+    user = EventsSpa.Users.authenticate(name, password)
+
+    if user do
+      sess = %{
+        user_id: user.id,
+        name: user.name,
+        token: Phoenix.Token.sign(conn, "user_id", user.id)
+      }
+      conn
+      |> put_resp_header("content-type", "application/json; charset=UTF-8")
+      |> send_resp(:created, Jason.encode!(sess))
+    else
+      conn
+      |> put_resp_header("content-type", "application/json; charset=UTF-8")
+      |> send_resp(:unauthorized, Jason.encode!(%{error: "fail"}))
+    end
   end
 end
