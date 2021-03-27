a45e576f776b284a6068e39f882d0c5e7f287bbe
nicolassims <sableye.nico@gmail.com>
2021-03-26 19:06:13 -0700

    Add deploy files.

---
+++ server/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export MIX_ENV=prod
+export port=8999
+
+CFGD=$(readlink -f ~/.config/events_app)
+
+if [ ! -e "$CFGD/base" ]; then
+	echo "run deploy first"
+	exit 1
+fi
+
+DP_PASS=$(cat "$CFGD/db_pass")
+export DATABASE_URL=ecto://events:$DP_PASS@localhost/events_spa_prod
+
+SECRET_KEY_BASE=$(cat "$CFGD/base")
+export SECRET_KEY_BASE
+
+_build/prod/rel/events_app/bin/events-spa start
